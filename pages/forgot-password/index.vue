780c79fb785f2b8e86b4c45ce982ffe0cb4759d0
Adrian Popescu <adrian.popescu@bkk-vbu.de>
2024-05-15 14:48:10 +0200

    Fix redirect on forgot-password page

--- pages/forgot-password/index.vue
+++ pages/forgot-password/index.vue
@@ -96,9 +96,9 @@ const resetPassword = handleSubmit(async (values, _ctx) => {
                     {{ $t(`forgot-password.instructions`) }}
                 </p>
                 <NuxtLink
-                    :to="localePath('/register')"
+                    :to="localePath('/')"
                     class="mt-10 inline-flex h-10 w-full items-center justify-center gap-2 rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2">
-                    {{ $t(`register.signIn`) }}
+                    {{ $t(`login.login`) }}
                 </NuxtLink>
             </div>
         </div>

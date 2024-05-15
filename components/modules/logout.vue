626d46fd39af62b79947b75408a2555c6f3ff111
Adrian Popescu <adrian.popescu@bkk-vbu.de>
2024-05-15 17:06:10 +0200

    Restyle logout

--- components/modules/logout.vue
+++ components/modules/logout.vue
@@ -1,36 +1,43 @@
 <script setup lang="ts">
 import {signOut} from "@firebase/auth";
 
+const {t} = useI18n();
 const localePath = useLocalePath();
-
-// Get the (authenticated) user from Firebase. Hide login form if user is already logged in.
-const user = await getCurrentUser();
 
 // Retrieves the Firebase authentication instance for use in creating and managing user authentication.
 const auth = useFirebaseAuth();
 
 // Handle the form validation and submission.
 const logoutUser = async () => {
+    const loading = useSonner.loading(`${t("loading")}...`, {
+        description: t(`logout.loading`)
+    });
     try {
         // Authenticate the user with email and password.
         await signOut(auth!);
 
+        useSonner.success(t(`logout.loadingSuccess`), {
+            id: loading
+        });
+
         // Redirect to a dedicated page.
         return await navigateTo({path: localePath("/"), replace: true});
     } catch (error) {
-        showError("An unexpected error occurred. Please try again.");
+        const message = error as Error;
+
+        useSonner.error(message, {
+            id: loading
+        });
     }
 };
 </script>
 
 <template>
-    <div v-if="user">
-        <form @submit.prevent="logoutUser">
-            <button
-                type="submit"
-                class="rounded bg-secondary px-4 py-2 text-white hover:bg-primary focus:outline-none">
-                {{ $t(`login.logout`) }}
-            </button>
-        </form>
-    </div>
+    <UiButton
+        type="submit"
+        variant="destructive"
+        class="rounded"
+        @click="logoutUser">
+        {{ $t(`login.logout`) }}
+    </UiButton>
 </template>

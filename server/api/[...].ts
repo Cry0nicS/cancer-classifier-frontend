79d9abce8620b94ab17c14929ec5930108819239
Adrian Popescu <adrian.popescu@bkk-vbu.de>
2024-05-20 16:37:42 +0200

    Improve error handling in API proxy

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -1,13 +1,17 @@
 import {joinURL} from "ufo";
 
 export default defineEventHandler(async (event) => {
-    // Get the proxy URL from RuntimeConfig.
-    const proxyUrl = useRuntimeConfig().apiUrl;
-    // Replace the "/api/" prefix with an empty string.
-    // Therefore, "/api/endpoint" becomes "endpoint".
-    const path = event.path.replace(/^\/api\//, "");
-    const target = joinURL(proxyUrl, path);
+    try {
+        // Get the proxy URL from RuntimeConfig.
+        const proxyUrl = useRuntimeConfig().apiUrl;
+        // Replace the "/api/" prefix with an empty string.
+        // Therefore, "/api/endpoint" becomes "endpoint".
+        const path = event.path.replace(/^\/api\//, "");
+        const target = joinURL(proxyUrl, path);
 
-    // Proxy it.
-    return proxyRequest(event, target);
+        // Proxy it.
+        return proxyRequest(event, target);
+    } catch (error) {
+        return createError({statusCode: 500, statusMessage: "Internal Server Error"});
+    }
 });
